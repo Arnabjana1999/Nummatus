8f984c35ac1dad44d65b5f887a081de75745b4f9
Arnabjana1999 <ajana47@gmail.com>
2019-07-29 21:21:59 +0530

    bash scripts run for 100 iterations

--- run-QuPRsim.sh
+++ run-QuPRsim.sh
@@ -1,12 +1,12 @@
 cargo build --release
-cargo run --release --bin QuPR 100 25 #-n 100
-cargo run --release --bin QuPR 100 50 #-n 100
-cargo run --release --bin QuPR 100 75 #-n 100
+cargo run --release --bin QuPR 100 25 -n 100
+cargo run --release --bin QuPR 100 50 -n 100
+cargo run --release --bin QuPR 100 75 -n 100
 
-cargo run --release --bin QuPR 1000 250 #-n 100
-cargo run --release --bin QuPR 1000 500 #-n 100
-cargo run --release --bin QuPR 1000 750 #-n 100
+cargo run --release --bin QuPR 1000 250 -n 100
+cargo run --release --bin QuPR 1000 500 -n 100
+cargo run --release --bin QuPR 1000 750 -n 100
 
-cargo run --release --bin QuPR 10000 2500 #-n 100
-cargo run --release --bin QuPR 10000 5000 #-n 100
-cargo run --release --bin QuPR 10000 7500 #-n 100
+cargo run --release --bin QuPR 10000 2500 -n 100
+cargo run --release --bin QuPR 10000 5000 -n 100
+cargo run --release --bin QuPR 10000 7500 -n 100
--- run-simplesim.sh
+++ run-simplesim.sh
@@ -1,12 +1,12 @@
 cargo build --release
-cargo run --release --bin simple 25 #-n 100
-cargo run --release --bin simple 50 #-n 100
-cargo run --release --bin simple 75 #-n 100
+cargo run --release --bin simple 25 -n 100
+cargo run --release --bin simple 50 -n 100
+cargo run --release --bin simple 75 -n 100
 
-cargo run --release --bin simple 250 #-n 100
-cargo run --release --bin simple 500 #-n 100
-cargo run --release --bin simple 750 #-n 100
+cargo run --release --bin simple 250 -n 100
+cargo run --release --bin simple 500 -n 100
+cargo run --release --bin simple 750 -n 100
 
-cargo run --release --bin simple 2500 #-n 100
-cargo run --release --bin simple 5000 #-n 100
-cargo run --release --bin simple 7500 #-n 100
+cargo run --release --bin simple 2500 -n 100
+cargo run --release --bin simple 5000 -n 100
+cargo run --release --bin simple 7500 -n 100
